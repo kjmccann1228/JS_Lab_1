faf5fcb3bb5ddbe245152e87ddd07c79d8edbb07
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-10-04 19:11:18 -0400

    Incomplete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,59 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function function1()
+{
+  var str = ""
+  for(var i = 1 ; i<11 ; i++)
+  {
+    str = str + i + "<br />";
+  }
+  display.innerHTML=str;
+}
+
+function function2()
+{
+  var str = "";
+  for(var i = 1 ; i <= 20 ; i++)
+  {
+    if(i % 2 === 0)
+    {
+      str = str + i + "<br />";
+    }
+  }
+  display.innerHTML=str;
+}
+
+function function7()
+{
+  var there = false;
+  while(!there)
+  {
+    var response=prompt("Are we there yet");
+    switch(response.toUpperCase())
+    {
+      case "YES":
+      there = true;
+      break;
+
+      default:
+      break;
+    }
+  }
+  display.innerHTML="Good!";
+}
+
+function function8()
+{
+  var outerString = "";
+  for(var i=1 ; i<6 ; i++)
+  {
+    var innerString = "";
+    for(var j=1 ; j<=i ; j++)
+    {
+      innerString += "*";
+    }
+    outerString += innerString + "<br />";
+  }
+  display.innerHTML=outerString;
 }
