aa9d03b8552e198fa4d9c863975ced242b92c5c1
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-10-08 14:34:51 -0400

    Parts 1-10

--- js/script.js
+++ js/script.js
@@ -25,6 +25,54 @@ function function2()
   display.innerHTML=str;
 }
 
+function function3()
+{
+  var str = ""
+  var counter = 1;
+  while (Math.pow(counter, 2) <= 100)
+  {
+    str += Math.pow(counter, 2) + "<br />";
+    counter++;
+  }
+  display.innerHTML=str;
+}
+
+function function4()
+{
+  var str = ""
+  for(var i=0 ; i<4 ; i++)
+  {
+    str += Math.ceil(Math.random() * 100) + "<br />";
+  }
+  display.innerHTML=str;
+}
+
+function function5()
+{
+  var upperLimit = prompt("Pick a positive integer.");
+  var str = "";
+  for(var i = 0 ; i<=upperLimit ; i++)
+  {
+    //str += "SWAG ";
+    if(i%2 === 0)
+    {
+      str += i + "<br />";
+    }
+  }
+  display.innerHTML = str;
+}
+
+function function6()
+{
+  var input = prompt("Pick number");
+  var str = "";
+  for(var i = 1 ; i<=input ; i++)
+  {
+    str += Math.pow(2, i) + "<br />";
+  }
+  display.innerHTML = str;
+}
+
 function function7()
 {
   var there = false;
@@ -57,4 +105,35 @@ function function8()
     outerString += innerString + "<br />";
   }
   display.innerHTML=outerString;
+}
+
+function function9()
+{
+  var str = ""
+  for(var i=1 ; i<= 4 ; i++)
+  {
+    var line = "|";
+    for(j = 1 ; j<=4 ; j++)
+    {
+      line += i*j + "|";
+    }
+    str += line + "<br />";
+  }
+  display.innerHTML = str;
+}
+
+function function10()
+{
+  var dimension = prompt("Enter your table square dimension");
+  var str = ""
+  for(var i=1 ; i<= dimension ; i++)
+  {
+    var line = "|";
+    for(j = 1 ; j<= dimension ; j++)
+    {
+      line += i*j + "|";
+    }
+    str += line + "<br />";
+  }
+  display.innerHTML = str;
 }
